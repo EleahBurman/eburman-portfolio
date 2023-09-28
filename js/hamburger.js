03b34f8bd49e1c04cd0a4747c46ebb65b80ae208
eleahburman <eleahjburman@gmail.com>
2023-09-28 14:59:22 -0400

    Remove mobile nav from computer view

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,5 +1,5 @@
 const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.mobile-nav');
+const mobile_menu = document.querySelector('.nav-mobile');
 
 menu_btn.addEventListener('click', function () {
   menu_btn.classList.toggle('is-active');
