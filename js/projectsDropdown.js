008fae499fa12fd9355d6df92d735a2fca894e8d
eleahburman <eleahjburman@gmail.com>
2023-10-16 13:08:57 -0400

    Add turning transition for down arrow for projects

--- js/projectsDropdown.js
+++ js/projectsDropdown.js
@@ -2,28 +2,32 @@ document.addEventListener("DOMContentLoaded", function() {
   const projectsDropdown = document.querySelector(".projects-dropdown");
   const dropdownContent = document.querySelector(".projects-dropdown-content");
   const arrow = document.querySelector(".arrow");
+  let isUp = false; // Track the current state
+
+  // Function to toggle the arrow icon and dropdown content
+  function toggleDropdown() {
+    if (isUp) {
+      dropdownContent.style.display = "none";
+      arrow.classList.remove("up");
+      arrow.classList.add("down");
+    } else {
+      dropdownContent.style.display = "block";
+      arrow.classList.remove("down");
+      arrow.classList.add("up");
+    }
+    isUp = !isUp; // Toggle the state
+  }
 
   // Toggle the dropdown on click
   projectsDropdown.addEventListener("click", function(event) {
     event.stopPropagation();
-
-    if (dropdownContent.style.display === "none" || dropdownContent.style.display === "") {
-      dropdownContent.style.display = "block";
-      arrow.classList.remove("fa-chevron-down");
-      arrow.classList.add("fa-chevron-up");
-    } else {
-      dropdownContent.style.display = "none";
-      arrow.classList.remove("fa-chevron-up");
-      arrow.classList.add("fa-chevron-down");
-    }
+    toggleDropdown();
   });
 
   // Close the dropdown if the user clicks outside of it
   window.addEventListener("click", function(event) {
     if (dropdownContent.style.display === "block" && !projectsDropdown.contains(event.target)) {
-      dropdownContent.style.display = "none";
-      arrow.classList.remove("fa-chevron-up");
-      arrow.classList.add("fa-chevron-down");
+      toggleDropdown();
     }
   });
 
