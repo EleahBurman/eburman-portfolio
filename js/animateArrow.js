e01447ddd33dbd98677b48642ebe715254eabfbc
eleahburman <eleahjburman@gmail.com>
2024-01-24 10:12:34 -0500

    Add budget buddy to portfolio

---
+++ js/animateArrow.js
@@ -0,0 +1,9 @@
+// Optional: You can use JavaScript to dynamically set the width of the arrow based on the distance between the two images.
+
+const pdfImage = document.getElementById('pdfImage');
+const csvImage = document.getElementById('csvImage');
+const arrow = document.getElementById('animatedArrow');
+
+const distance = csvImage.offsetLeft - (pdfImage.offsetLeft + pdfImage.offsetWidth);
+
+arrow.style.width = `${distance + 50}px`; 
